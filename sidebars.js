793716c777df0b707d3b671bd0511ecddb497c38
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2022-05-16 19:16:02 +0800

    Adjust 1.18.x index sidebar

--- sidebars.js
+++ sidebars.js
@@ -2,13 +2,23 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  '1.18.x': [
-    '1.18.x/preparations/README',
-    '1.18.x/concepts/README',
-    '1.18.x/block-item-objects/README',
-    '1.18.x/block-entity/README',
-    '1.18.x/world-gen/README',
-  ],
+  '1.18.x': [{
+    type: 'category',
+    label: '正山小种 - 1.18.x',
+    collapsed: false,
+    collapsible: false,
+    link: {
+      type: 'doc',
+      id: '1.18.x/README'
+    },
+    items: [
+      '1.18.x/preparations/README',
+      '1.18.x/concepts/README',
+      '1.18.x/block-item-objects/README',
+      '1.18.x/block-entity/README',
+      '1.18.x/world-gen/README',
+    ],
+  }],
 };
 
 module.exports = sidebars;
